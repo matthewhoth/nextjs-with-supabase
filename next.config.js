8619d94a2938192c1b5c0291921cb7636cc0839c
Matthew Hoth <matthewhoth@gmail.com>
2024-08-12 18:05:24 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,37 @@
 const nextConfig = {
     async headers() {
         return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+          {
+            source: '/:path*', // Apply to all routes
+            headers: [
+              {
+                key: 'X-Frame-Options',
+                value: 'ALLOWALL', 
+              },
+              {
+                key: 'Content-Security-Policy',
+                value: "frame-ancestors *",
+              },
+              {
+                key: 'Permissions-Policy',
+                value: "", // Allow all features by not setting any restrictions
+              },
+              {
+                key: 'Access-Control-Allow-Origin',
+                value: 'http://localhost:3001', 
+              },
+              {
+                key: 'Access-Control-Allow-Credentials',
+                value: 'true', 
+              },
+              {
+                key: 'Access-Control-Allow-Headers',
+                value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+              },
+            ],
+          },
+        ];
+      },
 }
 
 
